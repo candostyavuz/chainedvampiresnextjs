72932bd99ed73eebbd6b504b343aee1f8d7a8fe8
candostyavuz <cdost@alumni.sabanciuniv.edu>
2021-10-30 16:52:44 +0300

    DC icon added

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -10,7 +10,10 @@ const Footer = () => {
                 <Image src="/twitter.png" alt="twitterlogo" width={60} height={60} />
             </a>  
             <a href="https://medium.com/@chainedvampires/chained-vampires-reflectionary-nfts-on-avalanche-blockchain-f7167c37b452">
-                <Image src="/mediumicon.png" alt="twitterlogo" width={60} height={60} />
+                <Image src="/mediumicon.png" alt="mediumlogo" width={60} height={60} />
+            </a>
+            <a href="https://discord.gg/YF2H3dky">
+                <Image src="/dc.png" alt="dclogo" width={60} height={60} />
             </a>  
             <style jsx>{`
                 .footer{
