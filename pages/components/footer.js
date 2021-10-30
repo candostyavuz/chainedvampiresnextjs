427e0564620f506179b0acfcc370c28c0988ae0e
candostyavuz <cdost@alumni.sabanciuniv.edu>
2021-10-30 13:12:32 +0300

    medium icon added

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -8,7 +8,10 @@ const Footer = () => {
         <div className="footer"> 
             <a href="https://twitter.com/chainedvampires">
                 <Image src="/twitter.png" alt="twitterlogo" width={60} height={60} />
-            </a>   
+            </a>  
+            <a href="https://medium.com/@chainedvampires/chained-vampires-reflectionary-nfts-on-avalanche-blockchain-f7167c37b452">
+                <Image src="/mediumicon.png" alt="twitterlogo" width={60} height={60} />
+            </a>  
             <style jsx>{`
                 .footer{
                     background: linear-gradient(rgba(21, 4, 3, 0.747), rgba(63, 0, 0, 0.87));
